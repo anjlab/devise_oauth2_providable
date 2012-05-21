93a0fc1b08e029cf991cef5415d23194024a1ea7
Yury Korolev <yury.korolev@gmail.com>
2012-05-21 14:29:53 +0400

    Use devise 2.1.0

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :oauth2_providable, :oauth2_password_grantable, :oauth2_refresh_token_grantable, :oauth2_authorization_code_grantable
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         # OAuth2 devise modules
+         :oauth2_providable,
+         :oauth2_password_grantable,
+         :oauth2_refresh_token_grantable,
+         :oauth2_authorization_code_grantable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
